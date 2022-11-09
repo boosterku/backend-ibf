af5be63d6011ecadc0540c0acd41517a753a7b55
boosterku <booster.ku0000@gmailcom>
2022-11-09 07:07:49 +0000

    Second Commit

--- run.py
+++ run.py
@@ -1,11 +1,34 @@
 from flask import Flask
+from flask import Response
+#from flask_ngrok import run_with_ngrok #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku
+import json
+
+f = open('./anscombe.json')
+
+geodata = json.load(f)
 
 pelatihan_ibf_app = Flask(__name__)
-
+#run_with_ngrok(pelatihan_ibf_app) #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku  
 
 @pelatihan_ibf_app.route('/')
-def hello_world():
-    return 'Hello World!'
+def send_json_data():
+    return Response(response=json.dumps(geodata),
+                    status=200,
+                    mimetype="application/json")
+    
+@pelatihan_ibf_app.route('/other-route')
+def send_json_data_other():
+    geodataspec = geodata[2]
+    return Response(response=json.dumps(geodataspec),
+                    status=200,
+                    mimetype="application/json")
+    
+@pelatihan_ibf_app.route('/other-route-two')
+def send_json_data_other_two():
+    geodataspec = geodata[1]["Series"]
+    return Response(response=json.dumps(geodataspec),
+                    status=200,
+                    mimetype="application/json")
 
 
 if __name__ == '__main__':
