156738c927066711a393941511ae757a3f254ed4
boosterku <booster.ku0000@gmailcom>
2022-11-10 09:44:42 +0000

    Second Commit

--- run.py
+++ run.py
@@ -8,11 +8,78 @@ from flask_cors import CORS
 
 geo=[]
 
-pelatihan_ibf_app = Flask(__name__)
-CORS(pelatihan_ibf_app)
-#run_with_ngrok(pelatihan_ibf_app) #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku  
+main = Flask(__name__)
+CORS(main)
+#run_with_ngrok(main)
 
-@pelatihan_ibf_app.route('/add', methods=["POST"])
+fileAbout='./about.json'
+fileKI='./ki.geojson'
+fileRH='./rh.geojson'
+fileMSLP='./mslp.geojson'
+fileTP='./tp.geojson'
+
+def openJSON(file):
+  with open(file) as f:
+    content=json.load(f)
+  return content
+
+@main.route("/")
+def helloWorld():
+  about=openJSON(fileAbout)
+  return Response(response=json.dumps(about),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route("/ki")
+def displayKI():
+  ki=openJSON(fileKI)
+  return Response(response=json.dumps(ki),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route("/rh")
+def displayRH():
+  rh=openJSON(fileRH)
+  return Response(response=json.dumps(rh),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route("/mslp")
+def displayMSLP():
+  mslp=openJSON(fileMSLP)
+  return Response(response=json.dumps(mslp),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route("/tp")
+def displayTP():
+  tp=openJSON(fileTP)
+  return Response(response=json.dumps(tp),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route('/query')
+def display_query():
+    parameter = request.args.get('parameter')
+    value = request.args.get('value')
+    operator = request.args.get('operator')
+    
+    fileOpen=f"./{parameter}.geojson"
+    variabel=openJSON(fileOpen)
+    features=variabel["features"]
+  
+    if operator=='lebihdari':
+      dataquery = [p for p in features if p["properties"]["value"] > int(value)]
+    elif operator=='kurangdari':
+      dataquery = [p for p in features if p["properties"]["value"] < int(value)]
+    elif operator=='samadengan':
+      dataquery = [p for p in features if p["properties"]["value"] == int(value)]
+    
+    return Response(response=json.dumps(dataquery),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route('/add', methods=["POST"])
 def add_status():
     try:
       req = json.loads(request.data)
@@ -29,7 +96,7 @@ def add_status():
                     status=404,
                     mimetype="application/json")
 
-@pelatihan_ibf_app.route('/get', methods=["GET"])
+@main.route('/get', methods=["GET"])
 def send_status():
     try:
       return Response(response=json.dumps(geo),
@@ -44,7 +111,7 @@ def send_status():
                     status=404,
                     mimetype="application/json")
 
-@pelatihan_ibf_app.route('/query')
+@main.route('/query')
 def sent_status():                    
       tipe = request.args.get('tipe')
       category = request.args.get('category')
@@ -58,4 +125,4 @@ def sent_status():
 
 
 if __name__ == '__main__':
-    pelatihan_ibf_app.run()
+    main.run()
